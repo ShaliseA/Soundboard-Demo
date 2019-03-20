f7723d32bbd89b8e4c0f34c5124532c602b5dd47
Shalise Ayromloo <shaliseayromloo@shalises-macbook-air.local>
2019-03-20 18:53:04 -0500

    This is soundboard app with Star Wars sounds (may be customized with any other sounds you prefer). I have added a star background, a logo and customized fonts.

--- Soundboard Demo/ViewController.swift
+++ Soundboard Demo/ViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    let soundFilenames = ["swvader01", "nerfherder", "yodalaughing", "laughfuzzball"]
+    
+    var audioPlayers = [AVAudioPlayer]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        
+        //set up audio players
+        for sound in soundFilenames {
+            
+            do {
+                //try to do something
+                let url = Bundle.main.path(forResource: sound, ofType: "wav")
+                let audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: url!))
+                
+                audioPlayers.append(audioPlayer)
+            } //closes do
+            catch {
+                //catch the error that is thrown
+                audioPlayers.append(AVAudioPlayer())
+            } //closes catch
+            
+            
+            
+        } //closes the for-loop
+        
+        
+        
+        
+    } //closes viewDidLoad()
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        //Dispose of any resources that can be recreated
+    } //closes didReceiveMemoryWarning()
 
-
-}
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        //Get the audioPlayer that corresponds
+        let audioPlayer = audioPlayers[sender.tag]
+        audioPlayer.play()
+        
+        
+    } //closes buttonTapped()
+    
+} //closes class
 
